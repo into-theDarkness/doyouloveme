61c6ec374e543c17f684e88c36b61456551ae6f1
into-theDarkness <zzpeterpan0510@gmail.com>
2020-02-13 21:24:20 +0700

    change

--- loveyou.js
+++ loveyou.js
@@ -2,8 +2,8 @@ function Yes() {
     alert("Vậy là chú đồng ý rồi nhé ^^ !");
 }
 function No() {
-    let a = Math.round(Math.random() * 650);
-    let b=  Math.round(Math.random()* 650);
+    let a = Math.round(Math.random() * 550);
+    let b=  Math.round(Math.random()* 550);
     document.getElementById('btnNo').style.top = b + 'px';
     document.getElementById('btnNo').style.left = a + 'px';
 }
